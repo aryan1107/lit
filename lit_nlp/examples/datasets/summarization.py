9e5162bced3b33f9788a45f71578e8ce31e74ae5
Aryan Chaurasia <31214277+aryan1107@users.noreply.github.com>
2022-06-13 22:47:30 -0400

    tydi_demo.py
    tydi_demo.py for pytorch Python demo

--- lit_nlp/examples/datasets/summarization.py
+++ lit_nlp/examples/datasets/summarization.py
@@ -54,3 +54,28 @@ class CNNDMData(lit_dataset.Dataset):
         "document": lit_types.TextSegment(),
         "reference": lit_types.TextSegment(),
     }
+
+class TYDIQAData(lit_dataset.Dataset):
+  """TYDIQA dataset."""
+
+  def __init__(self, split="validation-en", max_examples=-1, max_seq_len=500):
+    """Dataset constructor, loads the data into memory."""
+    ds = tfds.load("tydi_qa", split=split)
+
+    # into datafrane to decode string
+    df = tfds.as_dataframe(ds.take(max_examples))
+    df['context'] = df['context'].str.decode("utf-8")
+    df['question'] = df['question'].str.decode("utf-8")
+
+    # populate this with data records
+    self._examples = [{
+      'context': row['context'],
+      'question': 'question: ' + row['question'],
+    } for _, row in df.iterrows()]
+
+  def spec(self) -> lit_types.Spec:
+    """Dataset spec, which should match the model"s input_spec()."""
+    return {
+        "context": lit_types.TextSegment(),
+        "question": lit_types.TextSegment(),
+    }
